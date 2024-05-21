5732f68bcadcace4d981aa9528b789b29294ed6c
marinamassuet <massuet.marina24@gmail.com>
2024-05-22 00:51:23 +0200

    index.js

---
+++ docs/index.js
@@ -0,0 +1,13 @@
+function startQuiz() {
+  // Rediriger l'utilisateur vers la première question du quiz
+  window.location.href = "question1.html";
+}
+function Quizz1() {
+  let hggsp = 0;
+  let ses = 0;
+
+  let svt = 0;
+  let math = 0;
+  let phys = 0;
+  let nsi = 0;
+  let hlp = 0;
