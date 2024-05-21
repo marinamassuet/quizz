22df7a58244924ada8c38ccd7fdb46594913daa8
marinamassuet <massuet.marina24@gmail.com>
2024-05-22 01:09:13 +0200

    script.js

---
+++ docs/script.js
@@ -0,0 +1,8 @@
+function startQuiz() {
+  // Rediriger l'utilisateur vers la première question du quiz
+  window.location.href = "question1.html";
+}
+function Quizz1() {
+  // Rediriger l'utilisateur vers la première question du quiz
+  window.location.href = "question2.html";
+}
